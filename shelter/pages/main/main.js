ed1be906714bbbf5d6cee41944fe1be26ed473a2
Srr233 <Sr233@yandex.ru>
2020-10-13 15:33:10 +0300

    feat: add adaptive 320 width

---
+++ shelter/pages/main/main.js
@@ -0,0 +1,10 @@
+const NAVIGATION = document.body.querySelector(".header__nav");
+const BURGER_BTN = document.body.querySelector(".header__burger");
+const A_LINKS = document.body.querySelectorAll("a");
+
+BURGER_BTN.addEventListener("click", e => {
+    const element = e.target.tagName == "BUTTON" ? e.target : e.target.parentElement;
+    Array.from(element.children).forEach(span => span.classList.toggle("active"));
+    Array.from(A_LINKS).forEach(link => link.classList.toggle("active"));
+    NAVIGATION.classList.toggle("active");
+})
